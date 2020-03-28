d83efb10d5cf08740064827a43f105df407ff9d7
Andrew J Kim <andrewk745@gmail.com>
2020-03-28 05:59:30 -0400

    added herocontainer component

--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import HomeHeroContainer from './Home/HomeHeroContainer'
 
 export const Home = (props) => {
     return (
-        <h1>THIS IS HOME</h1>
+        <div>
+            <HomeHeroContainer
+            title='WELCOME TO PHONEFIXIT.COM'
+            description='We fix your devices at our stores, your place or by mail'
+            />
+        </div>
+
     )
 }
