32699105cf0ce79bc923f0d45aad13a19147497a
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-03-30 20:25:32 -0400

    added small changes

--- src/components/screens/Home/HomeHeroContainer.jsx
+++ src/components/screens/Home/HomeHeroContainer.jsx
@@ -25,8 +25,8 @@ const HomeHeroContainer = (props) => {
         <section className="hero is-large has-bg-img">
             <div className="hero-body">
                 <div className="container has-text-centered">
-                    <h1 className="title is-1 has-text-white">Welcome to PHONEFIXIT</h1>
-                    <h2 className="subtitle is-1">We fix your devices at our stores, your place or by mail.</h2>
+                    <h1 className="title is-1 has-text-white has-text-weight-bold	">Welcome to PHONEFIXIT</h1>
+                    <h2 className="subtitle is-3 has-text-black">We fix your devices at our stores, your place or by mail.</h2>
                     <button className="button is-primary">Start Here</button>
                 </div>
             </div>
