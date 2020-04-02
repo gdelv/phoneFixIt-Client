7c6c56196351fd57541eb75e8eb0d2f104d534bd
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-04-02 19:26:34 -0400

    completed AboutLocations component

---
+++ src/components/screens/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AboutLocations from './About/AboutLocations'
+
+export const About = (props) => {
+    return (
+        <div>
+            <AboutLocations/>
+        </div>
+
+    )
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Route, Switch } from "react-router-dom";
 import { Home } from '../components/screens/Home';
+import { About }   from '../components/screens/About';
 
 export const Routes = (props) => {
     return (
         <Switch>
-            <Route path to='/'component={Home} />
+            <Route exact to='/about'component={About} />
+            <Route exact to='/'component={Home} />
             {/* <Route path to='/services'component={} /> */}
             {/* <Route path to='/mail-in'component={} /> */}
             {/* <Route path to='/book-online'component={} /> */}
-            {/* <Route path to='/about'component={} /> */}
             {/* <Route path to='/shop'component={} /> */}
         </Switch>
     )
