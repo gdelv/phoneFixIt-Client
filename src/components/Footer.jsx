61437fd5ef0abe037dcf8c0737c0b96cbe1c7121
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-04-06 21:41:24 -0400

    made changes on g comp

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     return (
         <footer class="footer is-flex">
             <div class="columns">
-                <div class="column">
+                <div class="column is-one-quarter">
                     <img
                         src="https://bulma.io/images/bulma-logo.png"
                         width="112"
@@ -14,21 +14,17 @@ export const Footer = () => {
                     />
                     <h6 class="subtitle is-6 is-size-7-mobile">© 2019 Phonefixit.com, Inc. All Rights Reserved</h6>
                 </div>
-                <div class="column">
-                    <h4 class="title is-4 is-size-7-mobile">Address</h4>
+                <div class="column is-half">
+                    <h4 class="title is-4 is-size-5-mobile">Address</h4>
 
-                    <h6 class="subtitle is-6 is-size-7-mobile is-spaced">94 South Broadway</h6>
-                    <h6 class="subtitle is-6 is-size-7-mobile is-spaced">Hicksville NY, 11801 USA</h6>
+                    <h6 class="subtitle is-6 is-size-6-mobile is-spaced">94 South Broadway Hicksville NY, 11801 USA</h6>
 
-                    <h6 class="subtitle is-6 is-size-7-mobile is-spaced ">208 Glen St.</h6>
-                    <h6 class="subtitle is-6 is-size-7-mobile is-spaced">Glen Cove, NY, 11542 USA</h6>
+                    <h6 class="subtitle is-6 is-size-6-mobile is-spaced ">208 Glen St. Glen Cove, NY, 11542 USA</h6>
                 </div>
-                <div class="column">
-                    <h4 class="title is-4 is-size-7-mobile ">Contact</h4>
-                    <h6 class="subtitle is-6 is-size-7-mobile is-spaced">Hicksville, NY</h6>
-                    <h6 class="subtitle is-6 is-size-7-mobile is-spaced">(516)-513-0800</h6>
-                    <h6 class="subtitle is-6 is-size-7-mobile is-spaced">Glen Cove, NY</h6>
-                    <h6 class="subtitle is-6 is-size-7-mobile is-spaced">(516)-801-6320</h6>
+                <div class="column is-one-third">
+                    <h4 class="title is-4 is-size-5-mobile ">Contact</h4>
+                    <h6 class="subtitle is-6 is-size-6-mobile is-spaced">Hicksville, NY (516)-513-0800</h6>
+                    <h6 class="subtitle is-6 is-size-6-mobile is-spaced">Glen Cove, NY (516)-801-6320</h6>
                 </div>
                 {/* <div class="column">
                 <h4 class="title is-4 is-size-7-mobile">Social</h4>
