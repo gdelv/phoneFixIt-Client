ce13b34c8278a39ea1c5fec40d6b158a7b1e84c1
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-04-07 15:53:24 -0400

    cleaned up routes and added default route to when route is not found

---
+++ src/components/screens/Default.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Default() {
+    return (
+        <div>
+            <h1 className="title has-text-centered">This route does not exist</h1>
+        </div>
+    )
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,17 +6,29 @@ import { Home } from "../components/screens/Home";
 import { MailInRepair } from "../components/screens/MailInRepair";
 import { FAQ } from '../components/screens/FAQ/FAQ'
 import { About }   from '../components/screens/About';
+import Default from "../components/screens/Default";
 
 export const Routes = props => {
   return (
     <Switch>
+      {/* Home Route (/) */}
         <Route exact path="/" component={Home} />
-      {/* <Route path to='/services'component={} /> */}
+
+      {/* Services Route (/services) */}
+        {/* <Route path to='/services'component={} /> */}
+
+      {/* Mail In Repair Route (/mail-in-repair) */}
         <Route exact path="/mail-in-repair" component={MailInRepair} />
+
       {/* <Route path to='/book-online'component={} /> */}
+      {/* About Route (/about) */}
         <Route exact path='/about'component={About} />
-      {/* <Route path to='/shop'component={} /> */}
+
+      {/* FAQ Route (/FAQ) */}
         <Route exact path='/FAQ' component={FAQ}/>
+
+      {/* Route Not Found (/) */}
+      <Route component={Default} />
 
     </Switch>
   );
