d0fbc0cfec17c65aa0a0b3f5611304d6ae826f34
Andrew J Kim <andrewk745@gmail.com>
2020-04-08 08:17:40 -0400

    backdrop and sidedrawer are functional and work

--- src/components/screens/SideDrawer/SideDrawer.jsx
+++ src/components/screens/SideDrawer/SideDrawer.jsx
@@ -1,20 +1,58 @@
 import React from 'react'
+import './Styles/SideDrawer.scss'
+import { Link } from 'react-router-dom'
+import Instagram from '../../images/instagram.png'
+import Youtube from '../../images/youtube.png'
+import Facebook from '../../images/facebook.png'
+// import SideDrawer from './screens/SideDrawer/SideDrawer'
 
 
 
 
 
+class SideDrawer extends React.Component {
+    constructor(props) {
+        super(props);
 
-const SideDrawer = (props) => {
-    let drawerClasses = 'side-drawer'
-    if(this.props.show) {
-       drawerClasses = 'side-drawer open'
+
+        this.state = {
+        }
     }
-    return (
-        <div className={drawerClasses}>
-            <h1>Hello, This works</h1>
-        </div>
-    )
+
+
+
+    render() {
+        let drawerClasses = 'side-drawer'
+        if (this.props.show) {
+            drawerClasses = 'side-drawer open'
+        }
+        return (
+            <div className={drawerClasses}>
+                <nav class="navbar" role="navigation" aria-label="main navigation">
+                    <div class="navbar-brand is-column">
+                        <div class="navbar-item">
+                            <Link to="/">Home</Link>
+                        </div>
+                        <div class="navbar-item">
+                            <Link to='/services'>Services</Link>
+                        </div>
+                        <div class="navbar-item">
+                            <Link to="/mail-in-repair">Mail-in Repair</Link>
+                        </div>
+                        <div class="navbar-item">
+                            <Link to="/FAQ">FAQ</Link>
+                        </div>
+                        <div class="navbar-item">
+                            <Link to="/about">About Us</Link>
+                        </div>
+                        <div class="navbar-item">
+                            <Link to="/Shop">Shop</Link>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
 }
 
 
