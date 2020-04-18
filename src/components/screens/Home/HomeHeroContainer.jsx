bb44870bc871a26f5b7050f6cdfdc6789ad7b516
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-04-18 17:45:59 -0400

    added mobile changes

--- src/components/screens/Home/HomeHeroContainer.jsx
+++ src/components/screens/Home/HomeHeroContainer.jsx
@@ -7,8 +7,8 @@ const HomeHeroContainer = props => {
     <section className="hero is-small is-info home-hero-image">
         <div className="hero-body">
         <div className="container has-text-centered">
-            <h1 className="title is-1 is-primary has-text-white">{props.title}</h1>
-            <h2 className="subtitle is-3 has-text-info has-text-weight-bold	">{props.description}</h2>
+            <h1 className="title is-size-1-fullhd is-size-2-tablet is-primary has-text-white">{props.title}</h1>
+            <h2 className="subtitle is-size-2-fullhd is-size-3-tablet has-text-info has-text-weight-bold	">{props.description}</h2>
             <HomeServiceDropdown/>
             {/* <button className="button is-link">Start Here</button> */}
         </div>
