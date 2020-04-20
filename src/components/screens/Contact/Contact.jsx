4950004ddb55a6b9c0950866d7f0c0b85d326cfd
Andrew J Kim <andrewk745@gmail.com>
2020-04-20 01:50:56 -0400

    styled contact page

--- src/components/screens/Contact/Contact.jsx
+++ src/components/screens/Contact/Contact.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import ContactForm from './ContactForm'
+import './Styles/Contact.scss'
 
 export default class Contact extends Component {
     render() {
         return (
-            <div className='columns'>
+            <div className='columns has-padding'>
                 <div className="column">
                     <figure className="image is-4by5">
                         <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.freepik.com%2Ffree-photo%2Fhand-holding-a-smartphone-with-blank-screen_1232-1091.jpg&f=1&nofb=1" alt="contact"/>
